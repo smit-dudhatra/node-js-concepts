0b57b19c3008bd6a0d31ef9fe1f8cc78bea80e89
smit-dudhatra <dudhatrasmit1808@gmail.com>
2024-10-20 19:56:24 +0530

    extra questions added

--- 3 - Understanding The Basics/app.js
+++ 3 - Understanding The Basics/app.js
@@ -2,6 +2,37 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.method, req.url, req.originalUrl);
+  // GET , / , undefined
+
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
+    server.close(() => {
+      console.log("good bye");
+    });
+
+    const body = `
+    <html>
+      <title>This is form page</title>
+        <body>
+          <form method="POST" action="/message">
+            <input type="text" name="message" />
+            <br/>
+            <br/>
+            <button type="submit">Send</button>
+          </form>
+        </body>
+    </html>
+    `;
+
+    res.setHeader("Content-Type", "text/html");
+    res.write(body);
+    res.end();
+  }
+
+  if (url === "message" && method === "POST") {
+  }
 });
 
 server.listen(3000, () => {
